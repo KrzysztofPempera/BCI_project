a9e91cc17d395b6e9470882eb2e139eeca506bfb
KrzysztofPempera <k.pempera@gmail.com>
2020-01-12 18:13:55 +0100

    Added blinking controls #notworking

--- Space-Invaders-Pygame/main.py
+++ Space-Invaders-Pygame/main.py
@@ -1,8 +1,84 @@
 import math
 import random
-
+import multiprocessing as mp
 import pygame
+import pandas as pd
+
+import blink as blk
+import filterlib as flt
+
 from pygame import mixer
+
+global mac_adress, SYMULACJA_SYGNALU
+
+#######################
+SYMULACJA_SYGNALU = True
+#######################
+
+mac_adress = 'd2:b4:11:81:48:ad'
+
+def blinks_detector(quit_program, blink_det, blinks_num, blink):
+    def detect_blinks(sample):
+        if SYMULACJA_SYGNALU:
+            smp_flted = sample
+        else:
+            smp = sample.channels_data[0]
+            smp_flted = frt.filterIIR(smp, 0)
+        #print(smp_flted)
+
+        brt.blink_detect(smp_flted, -38000)
+        if brt.new_blink:
+            if brt.blinks_num == 1:
+                connected.set()
+                print('CONNECTED. Speller starts detecting blinks.')
+            else:
+                blink_det.put(brt.blinks_num)
+                blinks_num.value = brt.blinks_num
+                blink.value = 1
+
+        #if quit_program.is_set():
+        #    if not SYMULACJA_SYGNALU:
+        #        print('Disconnect signal sent...')
+        #        board.stop_stream()
+
+    if __name__ == '__main__':
+        clock = pygame.time.Clock()
+
+        frt = flt.FltRealTime()
+        brt = blk.BlinkRealTime()
+
+        if SYMULACJA_SYGNALU:
+            df = pd.read_csv('dane_do_symulacji/data.csv')
+            for sample in df['signal']:
+                if quit_program.is_set():
+                    break
+                detect_blinks(sample)
+                clock.tick(200)
+            print('KONIEC SYGNAŁU')
+            quit_program.set()
+        # else:
+            # board = OpenBCIGanglion(mac=mac_adress)
+            # board.start_stream(detect_blinks)
+
+blink_det = mp.Queue()
+blink = mp.Value('i', 0)
+blinks_num = mp.Value('i', 0)
+connected = mp.Event()
+quit_program = mp.Event()
+
+proc_blink_det = mp.Process(
+    name='proc_',
+    target=blinks_detector,
+    args=(quit_program, blink_det, blinks_num, blink,)
+    )
+
+# rozpoczęcie podprocesu
+proc_blink_det.start()
+print('subprocess started')
+
+########################
+###       GAME       ###
+########################
 
 # Intialize the pygame
 pygame.init()
@@ -40,7 +116,7 @@ for i in range(num_of_enemies):
     enemyImg.append(pygame.image.load('Space-Invaders-Pygame/enemy.png'))
     enemyX.append(random.randint(0, 736))
     enemyY.append(random.randint(50, 150))
-    enemyX_change.append(5)
+    enemyX_change.append(3)
     enemyY_change.append(20)
 
 # Bullet
@@ -121,7 +197,8 @@ while running:
             running = False
 
         # if keystroke is pressed check whether its right or left
-            if event.key == pygame.K_SPACE:
+            if blink.value == 1:
+                blink.value = 0
                 if bullet_state is "ready":
                     bulletSound = mixer.Sound("laser.wav")
                     bulletSound.play()
@@ -144,10 +221,10 @@ while running:
 
         enemyX[i] += enemyX_change[i]
         if enemyX[i] <= 0:
-            enemyX_change[i] = 4
+            enemyX_change[i] = 3
             enemyY[i] += enemyY_change[i]
         elif enemyX[i] >= 736:
-            enemyX_change[i] = -4
+            enemyX_change[i] = -3
             enemyY[i] += enemyY_change[i]
 
         # Collision
@@ -175,3 +252,7 @@ while running:
     player(playerX, playerY)
     show_score(textX, testY)
     pygame.display.update()
+
+# Zakończenie podprocesów
+proc_blink_det.join()
+
